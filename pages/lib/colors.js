265c634e5f137e5d41873d1ca125f3211d39c135
Arunjith T Nair <arunjith.unofficial@gmail.com>
2023-12-11 01:43:25 +0530

    making changes to fix deployment

--- pages/lib/colors.js
+++ pages/lib/colors.js
@@ -1,1 +1,9 @@
+import React from "react";
+
 export const primary = '#008080';
+
+const ColorsPage = () => {
+    return <div>Colors Page</div>;
+};
+
+export default ColorsPage;
--- pages/lib/mongoose.js
+++ pages/lib/mongoose.js
@@ -1,10 +1,18 @@
+import React from "react";
+
 import mongoose from "mongoose";
 
 export function mongooseConnect() {
     const uri = process.env.MONGODB_URI;
-    if (mongoose.connection.readyState ===1) {
-        return mongoose.connection.asPromise
+    if (mongoose.connection.readyState === 1) {
+        return mongoose.connection.asPromise;
     } else {
         return mongoose.connect(uri);
     }
 }
+
+const MongoosePage = () => {
+    return <div>Mongoose Page</div>;
+};
+
+export default MongoosePage;
