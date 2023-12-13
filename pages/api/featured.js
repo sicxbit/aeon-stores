cd1554f9f2430ce2ef1aa3d962a7490cdcd8e92f
Arunjith T S <arunjith.unofficial@gmail.com>
2023-12-14 02:22:02 +0530

    Fixed the imports in api/featured

--- pages/api/featured.js
+++ pages/api/featured.js
@@ -1,19 +1,38 @@
-import { mongooseConnect } from "@/lib/mongoose";
-import FeaturedProduct from "@/models/FeaturedProduct";
+import mongoose from "mongoose";
+import { mongooseConnect } from "../lib/mongoose";
 
 export default async function handler(req, res) {
-  try {
-    await mongooseConnect();
+    try {
+        await mongooseConnect();
 
-    const featuredProduct = await FeaturedProduct.findOne();
+        if (req.method === "GET") {
+            // Fetch the featured product
+            const featuredProduct = await mongoose.connection.collection('featuredproducts').findOne();
 
-    if (!featuredProduct) {
-      return res.status(404).json({ error: "Featured product not found" });
+            if (!featuredProduct) {
+                return res.status(404).json({ error: "Featured product not found" });
+            }
+
+            const featuredProductId = featuredProduct.productId.toString();
+
+            return res.json({ success: true, featuredProduct: featuredProductId });
+        } else if (req.method === "POST") {
+            const { productId } = req.body;
+
+            if (!productId) {
+                return res.status(400).json({ error: "Product ID is required" });
+            }
+
+            const objectId = new mongoose.Types.ObjectId(productId);
+
+            await mongoose.connection.collection('featuredproducts').updateOne({}, { $set: { productId: objectId } }, { upsert: true });
+
+            return res.json({ success: true, featuredProduct: objectId });
+        } else {
+            return res.status(405).json({ error: "Method Not Allowed" });
+        }
+    } catch (error) {
+        console.error("Error handling request:", error);
+        return res.status(500).json({ error: "Internal Server Error" });
     }
-
-    return res.json(featuredProduct);
-  } catch (error) {
-    console.error("Error fetching featured product:", error);
-    return res.status(500).json({ error: "Internal Server Error" });
-  }
 }
